e9fa644f316d0a56b9f0b1d19df3a7cb4148a5f9
Manu Overa <manu.overa@gmail.com>
2024-10-29 08:13:09 +0100

    feat(NOTICKET): add switch to MJoFormElements type

--- src/types/litui.d.ts
+++ src/types/litui.d.ts
@@ -3,7 +3,8 @@ import { MjoColorPicker } from "../mjo-color-picker";
 import { MjoRadio } from "../mjo-radio";
 import { MjoSelect } from "../mjo-select";
 import { MjoSlider } from "../mjo-slider";
+import { MjoSwitch } from "../mjo-switch";
 import { MjoTextarea } from "../mjo-textarea";
 import { MjoTextfield } from "../mjo-textfield";
 
-export type MjoFormElements = MjoTextfield | MjoCheckbox | MjoRadio | MjoSelect | MjoTextarea | MjoSlider | MjoColorPicker;
+export type MjoFormElements = MjoTextfield | MjoCheckbox | MjoRadio | MjoSelect | MjoTextarea | MjoSlider | MjoColorPicker | MjoSwitch;
