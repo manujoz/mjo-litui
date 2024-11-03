7914b9904b2900b5da8ef84e24d98dc02112cbb8
Manu Overa <manu.overa@gmail.com>
2024-11-03 23:02:37 +0100

    build(NOTICKET): close release 0.0

--- src/utils/dropdown.ts
+++ src/utils/dropdown.ts
@@ -3,7 +3,7 @@ import { type DropdowContainer } from "../helpers/dropdow-container";
 const DROPDOWN_MARGIN = 15;
 
 export const getTopInTopPosition = ({ dropDown, container }: { dropDown: DropdowContainer; container: HTMLElement }): number => {
-    const top = container.getBoundingClientRect().top + window.scrollY - dropDown.offsetHeight - 5;
+    let top = container.getBoundingClientRect().top + window.scrollY - dropDown.offsetHeight - 5;
 
     if (dropDown.offsetHeight >= window.innerHeight - DROPDOWN_MARGIN * 2) {
         dropDown.style.maxHeight = `${window.innerHeight - DROPDOWN_MARGIN * 2}px`;
@@ -12,12 +12,20 @@ export const getTopInTopPosition = ({ dropDown, container }: { dropDown: Dropdow
         dropDown.style.maxHeight = "";
     }
 
-    if (top < window.scrollY) return getTopInBottomPosition({ dropDown, container });
+    if (top < window.scrollY) {
+        top = container.getBoundingClientRect().top + window.scrollY + container.offsetHeight + 5;
+
+        if (top + dropDown.offsetHeight > window.innerHeight + window.scrollY) {
+            return window.scrollY + DROPDOWN_MARGIN;
+        }
+
+        return getTopInBottomPosition({ dropDown, container });
+    }
 
     return top;
 };
 export const getTopInBottomPosition = ({ dropDown, container }: { dropDown: DropdowContainer; container: HTMLElement }): number => {
-    const top = container.getBoundingClientRect().top + window.scrollY + container.offsetHeight + 5;
+    let top = container.getBoundingClientRect().top + window.scrollY + container.offsetHeight + 5;
 
     if (dropDown.offsetHeight >= window.innerHeight - DROPDOWN_MARGIN * 2) {
         dropDown.style.maxHeight = `${window.innerHeight - DROPDOWN_MARGIN * 2}px`;
@@ -26,7 +34,15 @@ export const getTopInBottomPosition = ({ dropDown, container }: { dropDown: Drop
         dropDown.style.maxHeight = "";
     }
 
-    if (top + dropDown.offsetHeight > window.innerHeight + window.scrollY) return getTopInTopPosition({ dropDown, container });
+    if (top + dropDown.offsetHeight > window.innerHeight + window.scrollY) {
+        top = container.getBoundingClientRect().top + window.scrollY - dropDown.offsetHeight - 5;
+
+        if (top < window.scrollY) {
+            return window.scrollY + DROPDOWN_MARGIN;
+        }
+
+        return getTopInTopPosition({ dropDown, container });
+    }
 
     return top;
 };
@@ -49,11 +65,19 @@ export const getTopInMiddlePosition = ({ dropDown, container }: { dropDown: Drop
     return top;
 };
 export const getLeftInLeftPosition = ({ dropDown, container }: { dropDown: DropdowContainer; container: HTMLElement }): number => {
-    const left = container.getBoundingClientRect().left + window.scrollX - dropDown.offsetWidth - 5;
+    let left = container.getBoundingClientRect().left + window.scrollX - dropDown.offsetWidth - 5;
 
     if (dropDown.offsetWidth >= window.innerWidth - DROPDOWN_MARGIN * 2) return DROPDOWN_MARGIN;
 
-    if (left < window.scrollX) return getLeftInRightPosition({ dropDown, container });
+    if (left < window.scrollX) {
+        left = container.getBoundingClientRect().left + window.scrollX + container.offsetWidth + 5;
+
+        if (left + dropDown.offsetWidth > window.innerWidth + window.scrollX) {
+            return DROPDOWN_MARGIN;
+        }
+
+        return getLeftInRightPosition({ dropDown, container });
+    }
 
     return left;
 };
@@ -71,11 +95,17 @@ export const getLeftInCenterPOsition = ({ dropDown, container }: { dropDown: Dro
     return left;
 };
 export const getLeftInRightPosition = ({ dropDown, container }: { dropDown: DropdowContainer; container: HTMLElement }): number => {
-    const left = container.getBoundingClientRect().left + window.scrollX + container.offsetWidth + 5;
+    let left = container.getBoundingClientRect().left + window.scrollX + container.offsetWidth + 5;
 
     if (dropDown.offsetWidth >= window.innerWidth - DROPDOWN_MARGIN * 2) return DROPDOWN_MARGIN;
 
     if (left + dropDown.offsetWidth > window.innerWidth + window.scrollX) {
+        left = container.getBoundingClientRect().left + window.scrollX - dropDown.offsetWidth - 5;
+
+        if (left < window.scrollX) {
+            return DROPDOWN_MARGIN;
+        }
+
         return getLeftInLeftPosition({ dropDown, container });
     }
 
