15f2a19761bea82ced2d871b62324a65945b8e0c
Manu Overa <manu.overa@gmail.com>
2025-09-16 18:16:13 +0200

    feat(MJOLIT-0): fix mjo-button types

--- src/types/mjo-button.d.ts
+++ src/types/mjo-button.d.ts
@@ -1,18 +1,10 @@
 import { MjoButton } from "../mjo-button";
 
-// Button size types
 export type MjoButtonSize = "small" | "medium" | "large";
-
-// Button color types
 export type MjoButtonColor = "primary" | "secondary" | "success" | "info" | "warning" | "error";
-
-// Button variant types
 export type MjoButtonVariant = "default" | "ghost" | "dashed" | "link" | "text" | "flat";
-
-// Button type attribute
 export type MjoButtonType = "button" | "submit" | "reset" | "menu";
 
-// Button click event interface
 export interface MjoButtonClickEvent extends CustomEvent {
     detail: {
         element: MjoButton;
@@ -20,8 +12,6 @@ export interface MjoButtonClickEvent extends CustomEvent {
         originalEvent: MouseEvent | KeyboardEvent;
     };
 }
-
-// Button toggle event interface
 export interface MjoButtonToggleEvent extends CustomEvent {
     detail: {
         element: MjoButton;
@@ -29,22 +19,17 @@ export interface MjoButtonToggleEvent extends CustomEvent {
         previousState: boolean;
     };
 }
-
-// Button loading state change event interface
 export interface MjoButtonLoadingChangeEvent extends CustomEvent {
     detail: {
         element: MjoButton;
         loading: boolean;
     };
 }
-
-// Button focus events
 export interface MjoButtonFocusEvent extends CustomEvent {
     detail: {
         element: MjoButton;
     };
 }
-
 export interface MjoButtonBlurEvent extends CustomEvent {
     detail: {
         element: MjoButton;
